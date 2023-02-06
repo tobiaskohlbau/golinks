eabe91d30c76d09494eb7f15906fba229d14ff4d
Tobias Kohlbau <tobias@kohlbau.de>
2023-02-06 19:46:48 +0100

    create initial support for jwt and jwks
    Authorize users by jwks and jwt claims. This allows
    running golinks on public internet and use zero trust
    providers like pomerium in front of the application.
    
    Signed-off-by: Tobias Kohlbau <tobias@kohlbau.de>

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/tobiaskohlbau/golinks
 go 1.16
 
 require (
+	github.com/MicahParks/keyfunc v1.9.0 // indirect
 	github.com/go-chi/chi v1.5.4
+	github.com/golang-jwt/jwt/v4 v4.4.3 // indirect
 	go.etcd.io/bbolt v1.3.5
 )
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"embed"
 	"encoding/csv"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/fs"
@@ -10,7 +12,9 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/MicahParks/keyfunc"
 	"github.com/go-chi/chi"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/tobiaskohlbau/golinks/server"
 	bolt "go.etcd.io/bbolt"
 )
@@ -62,6 +66,9 @@ func importCSV(path string) error {
 
 func execute() error {
 	dev := flag.Bool("dev", false, "enable development mode")
+	jwksURL := flag.String("jwksURL", "", "JWKS endpoint.")
+	jwtHeaderName := flag.String("jwtHeaderName", "", "JWT Headername")
+	jwtClaimName := flag.String("jwtClaimName", "", "JWT Clainname")
 	flag.Parse()
 
 	if len(os.Args) > 1 && os.Args[1] == "import" {
@@ -91,12 +98,97 @@ func execute() error {
 	}
 
 	r := chi.NewRouter()
+	if *jwksURL != "" {
+		r.Use(jwtHandler(db, *jwksURL, *jwtHeaderName, *jwtClaimName))
+	}
 	r.Handle("/static/*", http.FileServer(http.FS(content)))
 
 	srv := server.New(db, content)
 	r.Mount("/", srv)
 
 	return http.ListenAndServe(":80", r)
+}
+
+func jwtHandler(db *bolt.DB, jwksURL string, headerName string, claimName string) func(http.Handler) http.Handler {
+	jwks, err := keyfunc.Get(jwksURL, keyfunc.Options{})
+	if err != nil {
+		panic(err)
+	}
+	err = db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte("users"))
+		if err != nil {
+			return fmt.Errorf("failed to create users bucket: %w", err)
+		}
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			jwtBase64 := r.Header.Get(headerName)
+
+			if jwtBase64 == "" {
+				http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+			}
+
+			token, err := jwt.Parse(jwtBase64, jwks.Keyfunc)
+			if err != nil {
+				log.Println(err)
+				http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+				return
+			}
+
+			claims := token.Claims.(jwt.MapClaims)
+			if err := claims.Valid(); err != nil {
+				log.Println(err)
+				http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+				return
+			}
+			username, ok := claims[claimName].(string)
+			if !ok {
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+
+			err = db.Update(func(tx *bolt.Tx) error {
+				bucket := tx.Bucket([]byte("users"))
+
+				userData := bucket.Get([]byte(username))
+				if userData == nil {
+					user := User{
+						Username: username,
+					}
+					data, err := json.Marshal(user)
+					if err != nil {
+						return fmt.Errorf("failed to marshal new user: %w", err)
+					}
+					err = bucket.Put([]byte(username), data)
+					if err != nil {
+						return fmt.Errorf("failed to store new user: %w", err)
+					}
+				}
+				var user User
+				err := json.Unmarshal(userData, &user)
+				if err != nil {
+					return fmt.Errorf("failed to unmarshal stored user: %w", err)
+				}
+
+				ctx := context.WithValue(r.Context(), "user", user)
+				next.ServeHTTP(w, r.WithContext(ctx))
+				return nil
+			})
+
+			if err != nil {
+				log.Println(err)
+				http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+				return
+			}
+		})
+	}
+}
+
+type User struct {
+	Username string
 }
 
 func main() {
